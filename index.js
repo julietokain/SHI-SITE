dd0dde1b7c7d4b3d26c0e1cb6fe9649372ae4334
Juliet Dickson <102545215+julietokain@users.noreply.github.com>
2022-07-26 13:44:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  function expansion() {
+    var x = document.getElementById("navBar");
+    if (x.className === "navbar") {
+      x.className += " expanded";
+    } else {
+      x.className = "navbar";
+    }
+  }
